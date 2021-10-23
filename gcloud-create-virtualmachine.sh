58cc82210ece921f4115e24639c64de808c2be3a
phuongvohuy9 <huyphuong4116@gmail.com>
2021-10-23 21:39:04 +0700

    SSH to VM with gcloud

--- gcloud-create-virtualmachine.sh
+++ gcloud-create-virtualmachine.sh
@@ -16,14 +16,14 @@ export ZONE= #<zone>
 echo $PROJECT_ID
 echo $ZONE
 
-#######################################################
-# Create Virtual machine
-# gcloud compute instances create --help
-# gcloud config --help
-#######################################################
+##############################################################################################################
+### Create Virtual machine - gcloud compute instances create
+### gcloud compute instances create --help
+### gcloud config --help
+##############################################################################################################
 
 
-gcloud compute instances create gcelab2 --machine-type n1-standard-2 --zone $ZONE
+gcloud compute instances create my-gcp-VM --machine-type n1-standard-2 --zone $ZONE
 
 # Check configuration
 gcloud config list
@@ -38,5 +38,20 @@ sudo apt-get install google-cloud-sdk
 #Enable the gcloud interactive mode:
 gcloud beta interactive
 
+# How to SSH to VM from gcloud shell?
+gcloud compute ssh my-gcp-VM --zone $ZONE
 
+#Inside the Linux VM - Update packages and install NGINX
+sudo su -
+apt-get update
+apt-get install nginx -y
+#Confirm that NGINX is running
 ps auwx | grep nginx
+
+
+#Create new VM
+gcloud compute instances create --help
+gcloud compute instances create my-gcp-VM --machine-type n1-standard-2 --zone us-central1-f
+# How to SSH to VM from gcloud shell?
+gcloud compute ssh my-gcp-VM --zone us-central1-f
+exit
