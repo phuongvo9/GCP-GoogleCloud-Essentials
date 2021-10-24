4c7c1ad247540a8a5389807fe79392c09a1ca05d
phuongvohuy9 <huyphuong4116@gmail.com>
2021-10-24 11:29:41 +0700

    Add windows vm file

--- gcloud-create-windowsvm.sh
+++ gcloud-create-windowsvm.sh
@@ -23,13 +23,14 @@ echo $ZONE
 # Create Windows Virtual Machine?
 gcloud compute images list
 
-gcloud compute instances create VM_NAME \
-    [--image=IMAGE | --image-family=IMAGE_FAMILY] \
-    --image-project=IMAGE_PROJECT
-    --machine-type=MACHINE_TYPE
-gcloud compute images describe IMAGE_NAME \
-    --project=IMAGE_PROJECT
-
+#####################################################
+#gcloud compute instances create VM_NAME \
+#   [--image=IMAGE | --image-family=IMAGE_FAMILY] \
+#   --image-project=IMAGE_PROJECT
+#   --machine-type=MACHINE_TYPE
+#gcloud compute images describe IMAGE_NAME \
+#    --project=IMAGE_PROJECT
+#####################################################
 
 # How to check the server instance is ready for an RDP connection?
 gcloud compute instances get-serial-port-output instance-1
