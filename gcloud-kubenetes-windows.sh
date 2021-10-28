3af1adc1568ae532b015d9b7ec1255e2d6ace276
Jack VO <j.vo@linkbynet.com>
2021-10-28 22:35:12 +0700

    update

--- gcloud-kubenetes-windows.sh
+++ gcloud-kubenetes-windows.sh
@@ -23,5 +23,63 @@ gcloud container clusters create hello-dotnet-cluster\
 # NAME                  LOCATION          MASTER_VERSION  MASTER_IP      MACHINE_TYPE    NODE_VERSION   NUM_NODES  STATUS
 # hello-dotnet-cluster  us-central1-a  1.14.10-gke.27   35.195.57.183  n1-standard-1  1.14.10-gke.27  2          RUNNING
 
+# check our installed version of Kubernetes
+kubectl version
+
+# create a file to set the version of the dotnet runtime in cloud shell
+    # ```
+    # cat <<EOF > ~/global.json
+    # { "sdk": { "version": "3.1.411" } }
+    # EOF
+    # ```
+
+dotnet --version
+
+#disable Telemetry coming from our new app
+export DOTNET_CLI_TELEMETRY_OPTOUT=1
+
+#Create a skeleton ASP.NET Core web app using the following dotnet command:
+dotnet new razor -o HelloWorldAspNetCore
+
+cd HelloWorldAspNetCore
+
+# Run app with docker
+dotnet run --urls=http://localhost:8080
+
+    ### Example Output
+        # Hosting environment: Production
+        # Content root path: /home/atameldev/HelloWorldAspNetCore
+        # Now listening on: http://[::]:8080
+        # Application started. Press Ctrl+C to shut down.
+
+###------Publish the ASP.NET Core app###
+
+dotnet publish -c Release
+cd bin/Release/netcoreapp3.1/publish
+
+###------ Package the ASP.NET Core app as a Docker container ###------
+touch Dockerfile
+
+nano Dockerfile
+
+        FROM gcr.io/google-appengine/aspnetcore:3.1
+        ADD ./ /app
+        ENV ASPNETCORE_URLS=http://*:${PORT}
+        WORKDIR /app
+        ENTRYPOINT [ "dotnet", "HelloWorldAspNetCore.dll" ]
 
 
+DEVSHELL_PROJECT_ID=<project_id>
+docker build -t gcr.io/$DEVSHELL_PROJECT_ID/hello-dotnet:v1 .
+
+#  test the image locally which runs a Docker container as a daemon on port 8080 from new container image
+
+docker run -d -p 8080:8080 gcr.io/$DEVSHELL_PROJECT_ID/hello-dotnet:v1
+
+# Open the application with port 8080
+
+# Verify that the app is running locally in a Docker container
+docker ps
+
+# Stop the running container
+docker stop <Container_ID>
