01608ace8a8ca85c402215942769f5cffd21b963
phuongvo9 <huyphuong4116@gmail.com>
2021-11-03 23:06:37 +0700

    update comment

--- mydeploy.yaml/big-query-getstarted.sh
+++ mydeploy.yaml/big-query-getstarted.sh
@@ -4,15 +4,7 @@
 
 
 # gsutil cp gs://cloud-training/gcpfci/access_log.csv access_log.csv
-# export LOCATION=US
-# gsutil mb -l $LOCATION gs://$DEVSHELL_PROJECT_ID
 
-# gsutil cp access_log.csv gs://$DEVSHELL_PROJECT_ID/access_log.csv
-
-# gsutil acl ch -u allUsers:R gs://$DEVSHELL_PROJECT_ID/access_log.csv
-
-# gsutil acl ch -u allUsers:R gs://$DEVSHELL_PROJECT_ID/access_log.csv
-# Updated ACL on gs://qwiklabs-gcp-02-bea85aa0572f/access_log.csv
 
 
 gsutil cp gs://cloud-training/gcpfci/access_log.csv access_log.csv
