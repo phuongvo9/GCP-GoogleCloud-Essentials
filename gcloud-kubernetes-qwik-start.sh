194ce274346298fb229da7f14cd77c09c48108e7
phuongvo9 <huyphuong4116@gmail.com>
2021-11-10 00:14:55 +0700

    m

--- gcloud-kubernetes-qwik-start.sh
+++ gcloud-kubernetes-qwik-start.sh
@@ -34,6 +34,3 @@ kubectl get service
 # http://[EXTERNAL-IP]:8080
 # Delete the cluster in Google Kubernetes Engine vid gcloud cmd
 gcloud container clusters delete my-cluster 
-
-
-# gcloud container clusters delete my-cluster #
