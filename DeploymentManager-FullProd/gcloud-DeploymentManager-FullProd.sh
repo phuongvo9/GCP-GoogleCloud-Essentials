1e257cda337cb51aaa45223863916ee5b62058c1
phuongvo9 <huyphuong4116@gmail.com>
2021-11-15 00:36:28 +0700

    RUN THE APPLICATION AFTER MODIFYING THE PARAMETERS

--- DeploymentManager-FullProd/gcloud-DeploymentManager-FullProd.sh
+++ DeploymentManager-FullProd/gcloud-DeploymentManager-FullProd.sh
@@ -9,13 +9,38 @@ virtualenv -p python3 venv
 # Activate virtual env:
 source venv/bin/activate
 
-####
-# Clone the Deployment Manager Sample Templates
-###
-
+#################################################################
+####### Clone the Deployment Manager Sample Templates
+#################################################################
 mkdir ~/dmsamples
 cd ~/dmsamples
 git clone https://github.com/GoogleCloudPlatform/deploymentmanager-samples.git
+
+#################################################################
+####### Explore the Sample Files
+#################################################################
+cd ~/dmsamples/deploymentmanager-samples/examples/v2
+
+cd nodejs/python
+
+gcloud compute zones list
+
+nano nodejs.yaml
+nano nodejs.py
+#################################################################
+####### RUN THE APPLICATION AFTER MODIFYING THE PARAMETERS
+#################################################################
+gcloud deployment-manager deployments create advanced-configuration --config nodejs.yaml
+gcloud compute forwarding-rules list
+
+
+
+#################################################################
+####### Create a Monitoring workspace
+#################################################################
+
+
+
 
 
 
