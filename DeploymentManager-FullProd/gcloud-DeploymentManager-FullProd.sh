816653e476cea80cb15311dc1a751bf7d2d17006
phuongvo9 <huyphuong4116@gmail.com>
2021-11-15 00:52:37 +0700

    create monitoring workspace

--- DeploymentManager-FullProd/gcloud-DeploymentManager-FullProd.sh
+++ DeploymentManager-FullProd/gcloud-DeploymentManager-FullProd.sh
@@ -43,4 +43,42 @@ gcloud compute forwarding-rules list
 
 
 
+#################################################################
+####### Create a Monitoring workspace
+#################################################################
 
+# Configure an uptime check and alert policy in Cloud Monitoring
+# Configure an alerting policy and notification
+# Configure a Dashboard with a Couple of Useful Charts
+
+# Create a test VM with ApacheBench
+
+sudo apt-get update
+sudo apt-get -y install apache2-utils
+
+ab -n 1000 -c 100 http://<Your_IP>:8080/
+
+ab -n 5000 -c 100 http://<Your_IP>:8080/
+
+ab -n 10000 -c 100 http://<Your_IP>:8080/
+
+ab -n 10000 -c 100 http://<Your_IP>:8080/
+ab -n 10000 -c 100 http://<Your_IP>:8080/
+
+# user01@instance-1:~$ ab -n 1000 -c 100 http://35.231.224.194:8080/
+# This is ApacheBench, Version 2.3 <$Revision: 1843412 $>
+# Copyright 1996 Adam Twiss, Zeus Technology Ltd, http://www.zeustech.net/
+# Licensed to The Apache Software Foundation, http://www.apache.org/
+
+# Benchmarking 35.231.224.194 (be patient)
+# Completed 100 requests
+# Completed 200 requests
+# Completed 300 requests
+# Completed 400 requests
+# Completed 500 requests
+# Completed 600 requests
+# Completed 700 requests
+# Completed 800 requests
+# Completed 900 requests
+# apr_socket_recv: Connection timed out (110)
+# Total of 934 requests completed
