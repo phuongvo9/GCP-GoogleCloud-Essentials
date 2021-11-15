400253180b6c1f5f36d034ca1e637b0aaa78afe8
phuongvo9 <huyphuong4116@gmail.com>
2021-11-15 11:53:20 +0700

    move to folder

---
+++ Site Reliability Troubleshooting with Cloud Monitoring APM/gcloud-Site-Reliability-Troubleshoot-Monitoring.sh
@@ -0,0 +1,25 @@
+## Cloud Monitoring to monitor GKE cluster infrastructure, Istio, and applications deployed on GKE
+
+### IMPLEMENT
+    # Create a GKE cluster
+
+    # Deploy a microservices application to it
+
+    # Define latency and error SLIs and SLOs for it
+
+    # Configure Cloud Monitoring to monitor your SLIs
+
+    # Deploy a breaking change to the application and use Cloud Monitoring to troubleshoot and resolve the issues that result
+
+    # Validate that your resolution addresses the SLO violation
+
+### Target: Learn
+
+    # How to deploy a microservices application on an existing GKE cluster
+
+    # How to select appropriate SLIs/SLOs for an application
+
+    # How to implement SLIs using Cloud Monitoring features
+
+    # How to use Cloud Trace, Profiler, and Debugger to identify software issues
+
