d4c2ffacd16633be1a40867a91e6ae00482c082a
phuongvo9 <huyphuong4116@gmail.com>
2021-11-15 13:43:54 +0700

    add k8s manifests

--- Site Reliability Troubleshooting with Cloud Monitoring APM/gcloud-Site-Reliability-Troubleshoot-Monitoring.sh
+++ Site Reliability Troubleshooting with Cloud Monitoring APM/gcloud-Site-Reliability-Troubleshoot-Monitoring.sh
@@ -46,3 +46,23 @@ ln -s ~/training-data-analyst/blogs/microservices-demo-1 ~/microservices-demo-1
 # Download and install skaffold:
 
 curl -Lo skaffold https://storage.googleapis.com/skaffold/releases/latest/skaffold-linux-amd64 && chmod +x skaffold && sudo mv skaffold /usr/local/bin
+
+# Install the app using #skaffold
+cd microservices-demo-1
+skaffold run
+
+# Confirm everything is running correctly
+
+kubectl get pods
+
+#Get the external IP of the application:
+export EXTERNAL_IP=$(kubectl get service frontend-external | awk 'BEGIN { cnt=0; } { cnt+=1; if (cnt > 1) print $4; }')
+
+# Check the running app
+curl -o /dev/null -s -w "%{http_code}\n"  http://$EXTERNAL_IP
+    #Output should be 200
+
+# Download the source and put the code in the Cloud Source Repo:
+
+./setup_csr.sh
+
