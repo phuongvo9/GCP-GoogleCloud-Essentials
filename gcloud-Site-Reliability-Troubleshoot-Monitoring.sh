25f351b1b5388bd332462e0408045e567455e528
phuongvo9 <huyphuong4116@gmail.com>
2021-11-15 11:46:55 +0700

    add SR Troubleshoot Monitoring file

---
+++ gcloud-Site-Reliability-Troubleshoot-Monitoring.sh
@@ -0,0 +1,12 @@
+# Create a GKE cluster
+
+# Deploy a microservices application to it
+
+# Define latency and error SLIs and SLOs for it
+
+# Configure Cloud Monitoring to monitor your SLIs
+
+# Deploy a breaking change to the application and use Cloud Monitoring to troubleshoot and resolve the issues that result
+
+# Validate that your resolution addresses the SLO violation
+
