ca37b907371af7563838ee3d08e3585854d97fad
phuongvo9 <huyphuong4116@gmail.com>
2021-11-22 13:27:18 +0700

    Set up a virtual environment

--- AppEngine/AppEngine-Qwikstart.sh
+++ AppEngine/AppEngine-Qwikstart.sh
@@ -2,3 +2,14 @@
 
 gcloud app create --project=$DEVSHELL_PROJECT_ID
 git clone https://github.com/GoogleCloudPlatform/python-docs-samples
+
+cd python-docs-samples/appengine/standard_python3/hello_world
+
+#
+sudo apt-get update
+sudo apt-get install virtualenv
+virtualenv -p python3 venv
+
+source venv/bin/activate
+pip install  -r requirements.txt
+
