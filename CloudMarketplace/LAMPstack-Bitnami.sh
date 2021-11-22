b17ceddc79f955dbea979e00766582b2d69386a9
phuongvo9 <huyphuong4116@gmail.com>
2021-11-22 10:56:51 +0700

    add LAMP Packaged by Bitnami

---
+++ CloudMarketplace/LAMPstack-Bitnami.sh
@@ -0,0 +1,5 @@
+# LAMP Packaged by Bitnami - Deployed with Cloud Marketplace (GCP) - formely Cloud Launcher
+
+
+cd /opt/bitnami
+sudo sh -c 'echo "<?php phpinfo(); ?>" > apache2/htdocs/phpinfo.php'
