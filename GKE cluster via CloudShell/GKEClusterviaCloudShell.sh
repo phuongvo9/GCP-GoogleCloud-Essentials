0507592130721c6db7b93a1e85fec5396b8b4778
phuongvo9 <huyphuong4116@gmail.com>
2021-11-24 16:55:33 +0700

    resize GKE cluster --num-nodes=4

--- GKE cluster via CloudShell/GKEClusterviaCloudShell.sh
+++ GKE cluster via CloudShell/GKEClusterviaCloudShell.sh
@@ -8,3 +8,8 @@ export my_zone=us-central1-a
 export my_cluster=standard-cluster-1
 # create a Kubernetes cluster.
 gcloud container clusters create $my_cluster --num-nodes 3 --zone $my_zone --enable-ip-alias
+
+
+### Modify GKE Clusters
+# resize
+gcloud container clusters resize $my_cluster --zone $my_zone --num-nodes=4
