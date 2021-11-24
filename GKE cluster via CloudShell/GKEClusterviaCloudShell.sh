f5fb2f544dfe96263fc89cff328e1a2415571d89
phuongvo9 <huyphuong4116@gmail.com>
2021-11-24 16:52:53 +0700

    create a Kubernetes cluster with gcloud

--- GKE cluster via CloudShell/GKEClusterviaCloudShell.sh
+++ GKE cluster via CloudShell/GKEClusterviaCloudShell.sh
@@ -3,3 +3,8 @@
 # Use kubectl and configuration files to deploy Pods
 
 # Use Container Registry to store and deploy containers
+
+export my_zone=us-central1-a
+export my_cluster=standard-cluster-1
+# create a Kubernetes cluster.
+gcloud container clusters create $my_cluster --num-nodes 3 --zone $my_zone --enable-ip-alias
